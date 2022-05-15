751997544239ec3e779581adfff266b12c7faac1
selvianu <selvianu@gmail.com>
2022-05-15 19:21:38 +0530

    Sonar analysis TaskApp WebApp

--- src/main/java/in/spinsoft/model/Users.java
+++ src/main/java/in/spinsoft/model/Users.java
@@ -2,16 +2,15 @@ package in.spinsoft.model;
 
 public class Users {
 
-	public Integer userId;
-	public String userName;
-	public String password;
+	public static Integer userId;
+	public static String userName;
+	public static String password;
 	public String email;
+
 	@Override
 	public String toString() {
 		return "Users [userId=" + userId + ", userName=" + userName + ", password=" + password + ", email=" + email
 				+ "]";
 	}
 
-	
-	
 }
